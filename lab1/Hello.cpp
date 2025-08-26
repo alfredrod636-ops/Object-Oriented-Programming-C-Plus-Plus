9ed3da53427981fecc5a54134cf3890c66d28abe
Alfred Rodriguez <alfredrod636@gmail.com>
2025-08-26 12:18:21 -0700

    lab 1

---
+++ lab1/Hello.cpp
@@ -0,0 +1,13 @@
+/*** 
+ * 
+ * */
+
+#include <iostream>
+#include <cstdlib>
+using namespace std; 
+
+int main() {
+  cout << "Hello world" << endl;
+ printf("Hello world again!\n");
+ return 0;
+}
